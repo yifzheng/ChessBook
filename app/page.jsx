017df865677c8e284a27af499f53261c88582b03
Yifeng Zheng <yifeng.zheng43@bcmail.cuny.edu>
2023-12-16 17:42:22 -0500

    modified bg-colors with custom colors

--- app/page.jsx
+++ app/page.jsx
@@ -11,7 +11,7 @@ export default function Home () {
         alt="App_logo"
         className="object-contain mix-blend-multiply"
       />
-      <h2 className="text-xl max-w-3xl">Welcome to ChessBook, a palce where passionate chess players can share their chess games in a global chess community</h2>
+      <h2 className="text-xl max-w-3xl">Welcome to ChessBook, a place where passionate chess players can share their chess games in a global chess community</h2>
       <section className="flex gap-4 mt-10">
         <button type="button" className="login_btn">Login</button>
         <button type="button" className="register_btn">Register</button>
