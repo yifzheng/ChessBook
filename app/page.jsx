80305188ebf8b2730f8edb275624f3e9724db420
Yifeng Zheng <yifeng.zheng43@bcmail.cuny.edu>
2023-12-16 18:21:17 -0500

    modified styling for main page

--- app/page.jsx
+++ app/page.jsx
@@ -1,20 +1,23 @@
+'use client'
+
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 import Logo from "../public/icons/chessbook_icon-transformed.png"
 
 export default function Home () {
+  const router = useRouter()
   return (
-    <main className="flex-center flex-col h-screen text-center">
+    <main className="flex flex-col justify-center items-center h-screen text-center">
       <Image
         src={ Logo }
-        width={ 500 }
-        height={ 600 }
+        width={ 500}
         alt="App_logo"
         className="object-contain mix-blend-multiply"
       />
-      <h2 className="text-xl max-w-3xl">Welcome to ChessBook, a place where passionate chess players can share their chess games in a global chess community</h2>
-      <section className="flex gap-4 mt-10">
-        <button type="button" className="login_btn">Login</button>
-        <button type="button" className="register_btn">Register</button>
+      <h2 className="title_text">Welcome to ChessBook, a place where passionate chess players can share their chess games in a global chess community</h2>
+      <section className="flex gap-4 my-10">
+        <button className="login_btn" onClick={ () => router.push("/login") }>Login</button>
+        <button className="register_btn" onClick={ () => router.push( '/register' ) }>Register</button>
       </section>
     </main>
   )
